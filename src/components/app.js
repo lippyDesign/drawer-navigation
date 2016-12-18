d0c18f67740845233e475e5e443303e4b8846112
Volodymyr Lipunov <volo_lipu@yahoo.com>
2016-12-18 15:45:42 -0800

    Made the animation

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-    state = {
-        whichDrawerOpen: ''
-    };
+    constructor() {
+        super();
+
+        this.state = {
+            whichDrawerOpen: '',
+            navButtonBars: 'fa fa-bars fa-3x',
+            navButtonCogs: 'fa fa-cogs fa-3x'
+        };
+
+        this.pageClick = this.pageClick.bind(this);
+    }
+    
+
+    componentDidMount() {
+        window.addEventListener('mousedown', this.pageClick, false);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('mousedown', this.pageClick, false);
+    }
 
     openLeftDrawer() {
-        this.setState({whichDrawerOpen: 'openLeft'});
+        this.setState({ whichDrawerOpen: 'openLeft', navButtonBars: 'fa fa-times fa-3x' });
     }
 
     openRightDrawer() {
-        this.setState({whichDrawerOpen: 'openRight'});
+        this.setState({whichDrawerOpen: 'openRight', navButtonCogs: 'fa fa-times fa-3x'});
+    }
+
+    pageClick(e) {
+        const { whichDrawerOpen } = this.state;
+
+        if (e.target.className === 'fa fa-bars fa-3x' || e.target.className === 'fa fa-cogs fa-3x') {
+            return
+        }
+
+        switch(whichDrawerOpen) {
+            case 'openLeft':
+                this.setState({ whichDrawerOpen: 'closeLeft', navButtonBars: 'fa fa-bars fa-3x' });
+                break;
+            case 'openRight':
+                this.setState({ whichDrawerOpen: 'closeRight', navButtonCogs: 'fa fa-cogs fa-3x' });
+                break;
+            default:
+                return;
+        }
     }
 
     render() {
-        const { whichDrawerOpen } = this.state;
+        const { whichDrawerOpen, navButtonBars, navButtonCogs } = this.state;
         return (
             <div className="drawerContainer">
                 <section className="sliderHolder">
@@ -40,12 +76,12 @@ export default class SearchBar extends Component {
                     <ul className="drawerControlsList">
                         <li className="leftLi">
                             <button onClick={this.openLeftDrawer.bind(this)}>
-                                <i className="fa fa-bars fa-3x" aria-hidden="true"></i>
+                                <i className={navButtonBars} aria-hidden="true"></i>
                             </button>
                         </li>
                         <li className="rightLi">
                             <button onClick={this.openRightDrawer.bind(this)}>
-                                <i className="fa fa-cogs fa-3x" aria-hidden="true"></i>
+                                <i className={navButtonCogs} aria-hidden="true"></i>
                             </button>
                         </li>
                     </ul>
